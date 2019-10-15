f8909e56b7fc7599e3147a37f5ac9b2350565d67
zach <zacharyzjp@gmail.com>
2019-10-15 23:34:35 +0800

    clean bootstrap provision when destroy

--- code/terraform/00-preface/hello-world/main.tf
+++ code/terraform/00-preface/hello-world/main.tf
@@ -19,17 +19,23 @@ variable "ssh_target_host" {
 
 resource "null_resource" "target_host" {
     
-    provisioner "remote-exec" {
-        inline = [ 
-        "pwd"   
-        ]
-
-        connection {
-        type = "ssh"
-        user = var.ssh_user
-        password = var.ssh_password
-        host = var.ssh_target_host
-        }
+    connection {
+    type = "ssh"
+    user = var.ssh_user
+    password = var.ssh_password
+    host = var.ssh_target_host
     }
 
+    provisioner "remote-exec" {
+        inline = [ 
+        "touch /tmp/terraform-bootstrap-provision.txt"   
+        ]
+    }
+
+    provisioner "remote-exec" {
+	when  = "destroy"
+        inline = [ 
+        "rm -rf /tmp/terraform-bootstrap-provision.txt"   
+        ]
+    }
 } 
